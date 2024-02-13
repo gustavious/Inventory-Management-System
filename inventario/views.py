1855c32d4b36a8c996ccfebf7d3c8bb81a5cbebf
Gustavo Alegria <gusalegria18@gmial.com>
2024-02-13 17:44:42 -0500

    chore: add views definition

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Equipo
+from .models import EquipoUsuario
+from django.contrib import admin
+
+
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'referencia', 'marca', 'tipo')
+    list_filter = ('tipo',)
+    search_fields = ('referencia',)
+
+
+@admin.register(EquipoUsuario)
+class EquipoUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'equipo', 'usuario', 'fecha_asignacion')
+    list_filter = ('fecha_asignacion', 'usuario')

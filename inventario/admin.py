8b5d6be45a3209d8907f3130ea118bb2b15d03ae
Gustavo Alegria <gusalegria18@gmial.com>
2024-02-13 19:47:18 -0500

    fix: move admin views to work properly

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from inventario.models import Equipo, EquipoUsuario
+
+
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'referencia', 'marca', 'tipo')
+    list_filter = ('tipo',)
+    search_fields = ('referencia',)
+
+
+@admin.register(EquipoUsuario)
+class EquipoUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'equipo', 'usuario', 'fecha_asignacion')
+    list_filter = ('fecha_asignacion', 'usuario')
--- inventario/views.py
+++ inventario/views.py
@@ -8,20 +8,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import EquipoSerializer
 
-
-@admin.register(Equipo)
-class EquipoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'referencia', 'marca', 'tipo')
-    list_filter = ('tipo',)
-    search_fields = ('referencia',)
-
-
-@admin.register(EquipoUsuario)
-class EquipoUsuarioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'equipo', 'usuario', 'fecha_asignacion')
-    list_filter = ('fecha_asignacion', 'usuario')
-
-
 class EquipoListView(APIView):
     def get(self, request):
         equipos = Equipo.objects.all()
